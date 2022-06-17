c454e8c271c69120950f67602f4eb475abb07494
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2022-06-17 16:57:50 +0530

    nav-bar-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './card.js'
 
 function App() {
   return (
     <div>
       <h2>Hello</h2>
+      <Card></Card>
     </div>
   );
 }
