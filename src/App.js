6d093ea53d652f3aece0baaa0f7d2a7cf0690aba
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2022-06-17 23:38:48 +0530

    halfway to bottom

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Nav from './nav-bar.js'
 import Content from './content.js';
+import Footer from './bottom.js';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <Content></Content>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/bottom.js
@@ -0,0 +1,12 @@
+function Footer(){
+    return(
+        <footer className = "pt-4 my-md-5 pt-md-5 border-top foots">
+            <div className = "row">
+                <div className = "col-12 col-md">
+                    <img className = "mb-2" src = "https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt width="24" height="24"></img>
+                </div>        
+            </div> 
+        </footer>
+    )
+}
+export default Footer;
