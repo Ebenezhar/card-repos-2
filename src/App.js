fb999fcbe907b62caf33f9d164a86bf247f82657
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2022-06-18 23:18:07 +0530

    Halfway

--- src/App.js
+++ src/App.js
@@ -3,16 +3,102 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Nav from './nav-bar.js'
 import Content from './content.js';
-import Footer from './bottom.js';
+
 
 function App() {
+  let nav_inputs ={
+    name:"company Name",
+    list: [
+      {
+        name:"Features",
+        isEnable: true,
+      },
+      {
+        name:"Enterprise",
+        isEnable: true,
+      },
+      {
+        name:"Support",
+        isEnable: true,
+      },
+      {
+        name:"Pricing",
+        isEnable: true,
+      }
+    ],
+    btn:"Sign up"
+  }
+  let content_inputs = 
+    {
+      price: "Pricing",
+      content: "Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.",
+      boxes:[
+        {
+          head:"Free",
+          charge: 0,
+          addit:[
+            {
+              fae: "10 users included",
+            },
+            {
+              fae: "2 GB of storage",
+            },
+            {
+              fae: "Email support",
+            },
+            {
+              fae: "Help center access",
+            }
+          ],
+          btn: "Sign up for free",
+        },
+        {
+          head:"Pro",
+          charge: 15,
+          addit:[
+            {
+              fae: "20 users included",
+            },
+            {
+              fae: "10 GB of storage",
+            },
+            {
+              fae: "Priority email support",
+            },
+            {
+              fae: "Help center access",
+            }
+          ],
+          btn: "Get started",
+        },
+        {
+          head:"Enterprise",
+          charge: 29,
+          addit:[
+            {
+              fae: "30 users included",
+            },
+            {
+              fae: "15 GB of storage",
+            },
+            {
+              fae: "Phone and email support",
+            },
+            {
+              fae: "Help center access",
+            }
+          ],
+          btn: "Contact us",
+        }
+      ]
+     
+    }
+  
   return (
-    <div>
-      <Nav></Nav>
-      <Content></Content>
-      <Footer></Footer>
-    </div>
+    <div>      
+      <Nav data = {nav_inputs}></Nav>
+      <Content data = {content_inputs}></Content>    
+    </div>    
   );
 }
-
 export default App;
--- src/nav-bar.js
+++ src/nav-bar.js
@@ -1,14 +1,16 @@
-function Nav(){
+function Nav({data}){
+    // console.log("prp",data);
     return(
-        <div className = "d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-            <h5 className = "my-0 mr-md-auto font-weight-normal"> Company Name</h5>  
+        <div className = "d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow nav-bar boxes">
+            <h5 className = "my-0 mr-md-auto font-weight-normal compName"> {data.name}</h5>  
             <nav className = "my-2 my-md-0 mr-md-3">
-                <a className = "p-2 text-dark" href = "#">Features</a>
-                <a className = "p-2 text-dark" href = "#">Enterprise</a>
-                <a className = "p-2 text-dark" href = "#">Support</a>
-                <a className = "p-2 text-dark" href = "#">Pricing</a>    
+                {
+                    data.list.map((feature)=>{
+                        return <a className = "p-2 text-dark" href = "#">{feature.name}</a>
+                    })
+                }   
             </nav> 
-            <a className = "btn btn-outline-primary" href = "#">Sign up</a>
+            <a className = "btn btn-outline-primary" href = "#">{data.btn}</a>
         </div>
     )
 }
